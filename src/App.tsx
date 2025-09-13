4ec2c13d390338c6027582f0e3fa0211ba9423cd
abrown5421 <augustashtonbrown@gmail.com>
2025-09-13 17:02:15 -0400

    Initializing the react project, installing packages, and modifying the configs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'rkitech-components';
+
+const App: React.FC = () => {
+
+  return (
+    <Container tailwindClasses='text-3xl font-bold underline'>
+      App
+    </Container>
+  );
+};
+export default App;
