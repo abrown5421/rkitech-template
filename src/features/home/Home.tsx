1be83399084744cc1c2e9e76e09ef489445cce70
abrown5421 <augustashtonbrown@gmail.com>
2025-09-17 13:58:11 -0400

    more props needed for the page shell, adding them into the cli branch

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { Container } from 'rkitech-components';
-import { useAppSelector } from '../../app/hooks';
 
 const Home: React.FC = () => {
-    const activePage = useAppSelector((state) => state.activePage);
 
     return (
-        <Container 
-            animationObject={{
-                entranceAnimation: 'animate__fadeInUpBig',
-                exitAnimation: 'animate__fadeOutDownBig',
-                isEntering: activePage.activePageIn && activePage.activePageName === 'Home'
-            }}
-            tailwindClasses='h-[calc(100vh-54px)] bg-gray-50 p-5'
-        >
-            <Container tailwindClasses='flex-row gap-4 h-[50px]'>
-                Welcome To Rkitech
-            </Container>
+        <Container tailwindClasses=''>
+            Welcome To Rkitech
         </Container>
     );
 };
