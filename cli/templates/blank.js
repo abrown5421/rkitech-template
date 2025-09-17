fde0fa22dcdbb3207057a62f67428bdabe7afffe
abrown5421 <augustashtonbrown@gmail.com>
2025-09-17 17:20:36 -0400

    removing unecessary stuff from the blank template

--- cli/templates/blank.js
+++ cli/templates/blank.js
@@ -1,11 +1,8 @@
 export const blankTemplate = (componentName, folderName) => `import React from 'react';
 import { Container } from 'rkitech-components';
-import { useAppSelector } from '../../app/hooks';
 import type { ${componentName}Props } from './${folderName}Types';
 
 const ${componentName}: React.FC<${componentName}Props> = () => {
-    const activePage = useAppSelector((state) => state.activePage);
-
     return (
         <Container 
             tailwindClasses=''
